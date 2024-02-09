cc0ec19497eb566f035d279d141d78849d8b4d14
kutlumbek <Moralek228@gmail.com>
2024-02-09 11:02:38 +0300

    php 1.4 - 1.10

--- php/code_mu.php
+++ php/code_mu.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 // https://code.mu/ru/php/tasker/stager/
 #1.1
 #1.1
@@ -28,7 +29,7 @@ if ($number & 1) {
 //    echo 'число нечетное';
 }
 
-if (\bcmod($number, '2') == 0) {
+if (\bcmod((string)$number, '2') == 0) {
 //    echo 'число четное';
 }
 
@@ -81,12 +82,200 @@ if ($firstDigitOne === $firstDigitTwo) {
 #1.1
 
 if (\strlen($str) > 1) {
-    echo \substr($str, -2);
+//    echo $str[\strlen($str) - 2];
 }
 
+#1.2
+if (4 % 2 === 0) {
+//    echo 'Первое число без остатка делится на второе';
+}
+
+#------------------------------------------
+#1.4
+#1.1
+foreach (range(1, 100) as $value) {
+//    echo $value;
+}
+#1.2
+foreach (range(-100, 0) as $value) {
+//    echo $value;
+}
+#1.3
+foreach (range(100, 1) as $value) {
+//    echo $value;
+}
+#1.4
+foreach (range(1, 100) as $value) {
+    if ($value % 2 === 0) {
+//        echo $value . "\n";
+    }
+}
+#1.5
+foreach (range(1, 100) as $value) {
+    if ($value % 3 === 0) {
+//        echo $value . "\n";
+    }
+}
+#--------------------------------------
+#1.5
+#1.1
+$acc = 0;
+foreach (range(1, 100) as $value) {
+    $acc += $value;
+}
+#1.2
+$acc = 0;
+foreach (range(1, 100) as $value) {
+    if ($value % 2 === 0) {
+        $acc += $value;
+    }
+}
+#1.3
+$acc = 0;
+foreach (range(1, 100) as $value) {
+    if ($value % 2 !== 0) {
+        $acc += $value;
+    }
+}
+#1.4
+//---
+#1.5
+//echo strrev('hello');
+$str = 'hello';
+for ($i = strlen($str); $i >= 0; $i--) {
+    $char = substr($str, $i, 1);
+//    echo $char;
+}
+#--------------------------
+#1.6
+#1.1
+$double = function($a) {
+    return $a * $a;
+};
+
+$result = \array_map($double, [1, 2, 3, 4]);
+$acc = 0;
+foreach ($result as $value) {
+    $acc += $value;
+}
+//echo $acc;
+#1.2
+$sqrt = function($a) {
+    return \sqrt($a);
+};
+
+$result = \array_map('sqrt', [1, 2, 3, 4]);
+$acc = 0;
+//foreach ($result as $value) {
+//    $acc += $value;
+//}
+//foreach ([1, 2, 3, 4] as $value) {
+//    $acc += \sqrt($value);
+//}
+//echo $acc;
+
+#1.3
+$arr = [1, 2, -3, 4, -10];
+$acc = 0;
+foreach ($arr as $value) {
+    if ($value < 0) {
+        continue;
+    }
+
+    $acc += $value;
+}
+
+//echo $acc;
 
 
+#1.4
+$arr = [1, -3, 4, 11];
+$acc = 0;
+foreach ($arr as $value) {
+    if ($value > 0 && $value < 10) {
+        $acc += $value;
+    }
+}
+//echo $acc;
 
+#----------------------------------
+#1.7
+#1
+$arr = \str_split('abcde');
+#2
+$ints = 12345;
+//var_dump(array_map('intval', str_split($ints))[0]);
+#3
+//echo (\strrev((string)12345));
+#4
+$str = (string)12345;
+$acc = 0;
+for ($i = 0; $i < strlen($str); $i++) {
+    $acc += $str[$i];
+}
 
-
+//echo $acc;
+#----------------------
+#1.8
+#1
+$arr = range(1, 10);
+#2
+$arr = [];
+for ($i = 0; $i <= 100; $i++) {
+    if ($i % 2 === 0) {
+        $arr[] = $i;
+    }
+}
+#3
+$arr = [];
+foreach ([1.456, 2.125, 3.32, 4.1, 5.34] as $value) {
+    $arr[] = round($value, 1);
+}
+#---------------------------
+#1.9
+#1
+$arr = ['helo', 'http://hello'];
+foreach ($arr as $key => $value) {
+    if (!\str_starts_with($value, 'http://')) {
+        unset($arr[$key]);
+    }
+}
+#2
+$arr = ['helo', 'http://hello', 'hello.html'];
+foreach ($arr as $key => $value) {
+    if (!\str_ends_with($value, '.html')) {
+        unset($arr[$key]);
+    }
+}
+#3
+$arr = [3, 2, 1];
+foreach ($arr as &$value) {
+//    $value += $value * .1;
+//    $value *= 1.1;
+}
+//print_r($arr);
+#-------------------------------------
+#1.10
+#1
+$randomNumbers = \array_map(function() {
+    return \rand(1, 100);
+}, array_fill(0, 100, null));
+//print_r($randomNumbers);
+#2
+$str = 12345;
+settype($str, 'string');
+for ($i = strlen($str); $i >= 0; $i--) {
+//    echo \substr($str, $i, 1);
+}
+#3
+$arr = [1, 2, 3, 4, 5, 6];
+for ($i = 0; $i < count($arr); $i += 2) {
+    $subArray[] = \array_slice($arr, $i, 2);
+}
+#4
+for ($i = 0; $i < count($subArray); $i++) {
+    foreach ($subArray[$i] as $value) {
+        $newArr[] = $value;
+    }
+}
 
